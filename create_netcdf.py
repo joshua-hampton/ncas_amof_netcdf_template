bcc528bfca5d954576a6ce1829eeb56ef047714d
Joshua Hampton <j.m.hampton@leeds.ac.uk>
2022-05-09 14:29:36 +0100

    Initial upload

---
+++ create_netcdf.py
@@ -0,0 +1,182 @@
+import tsv2dict
+from netCDF4 import Dataset
+import datetime as dt
+import pytz
+import copy
+
+import values
+
+
+
+def add_attributes(ncfile, instrument_dict, product, created_time, location):
+    for key,value in instrument_dict['common']['attributes'].items():
+        if value['Fixed Value'] != '':
+            ncfile.setncattr(key, value['Fixed Value'])
+        elif key == 'source':
+            ncfile.setncattr(key, instrument_dict['info']['Descriptor'])
+        elif key == 'institution':
+            ncfile.setncattr(key, 'National Centre for Atmospheric Science (NCAS)')
+        elif key == 'platform':
+            ncfile.setncattr(key, location)
+        elif key == 'instrument_manufacturer':
+            ncfile.setncattr(key, instrument_dict['info']['Manufacturer'])
+        elif key == 'instrument_model':
+            ncfile.setncattr(key, instrument_dict['info']['Model No.'])
+        elif key == 'instrument_serial_number':
+            ncfile.setncattr(key, instrument_dict['info']['Serial Number'])
+        elif key == 'amf_vocabularies_release':
+            ncfile.setncattr(key, f'https://github.com/ncasuk/AMF_CVs/releases/tag/{values.TAG}')
+        elif key == 'history':
+            ncfile.setncattr(key, f'{created_time} - File created')
+        elif key == 'last_revised_date':
+            ncfile.setncattr(key, created_time)
+        else:
+            ncfile.setncattr(key, f"CHANGE: {value['Description']}. {value['Compliance checking rules']}")
+    
+    for key,value in instrument_dict[product]['attributes'].items():
+        if value['Fixed Value'] != '':
+            ncfile.setncattr(key, value['Fixed Value'])
+        else:
+            ncfile.setncattr(key, f"CHANGE: {value['Description']}. {value['Compliance checking rules']}")
+            
+            
+            
+def add_dimensions(ncfile, instrument_dict, product, dimension_lengths):
+    for key,value in instrument_dict['common']['dimensions'].items():
+        if key in dimension_lengths.keys():
+            length = dimension_lengths[key]
+        elif value['Length'] == '1':
+            length = 1
+        else:
+            msg = f'Cannot find length for dimension {key}'
+            raise ValueError(msg)
+        ncfile.createDimension(key, length)
+    for key,value in instrument_dict[product]['dimensions'].items():
+        if key in dimension_lengths.keys():
+            length = dimension_lengths[key]
+        elif value['Length'] == '1':
+            length = 1
+        else:
+            msg = f'Cannot find length for dimension {key}'
+            raise ValueError(msg)
+        ncfile.createDimension(key, length)
+        
+        
+def add_variables(ncfile, instrument_dict, product):
+    for obj in [product, 'common']:
+        for key, value in instrument_dict[obj]['variables'].items():
+            # therefore, value is instrument_dict[obj]['variables'][key]
+            # want to pop certain things here, but not for ever, so make tmp_value
+            tmp_value = copy.copy(value)
+            
+            var_dims = tmp_value['dimension']
+            # there was an error somewhere meaning 2 dimensions had a '.' instead of ',' between them
+            var_dims = var_dims.replace('.',',')
+            var_dims = tuple(x.strip() for x in var_dims.split(','))
+            
+            datatype = tmp_value.pop('type')
+            
+            if '_FillValue' in tmp_value:
+                fill_value = float(tmp_value.pop('_FillValue'))
+            else:
+                fill_value = ''
+                
+            if fill_value != '':
+                var = ncfile.createVariable(key, datatype, var_dims, fill_value = fill_value)
+            else:
+                var = ncfile.createVariable(key, datatype, var_dims)
+                
+            for mdatkey, mdatvalue in tmp_value.items():
+                var.setncattr(mdatkey, mdatvalue)
+    
+
+            
+
+def make_netcdf(instrument, product, time, instrument_dict, dimension_lengths = {}, **kwargs):
+    """
+    dimension_lengths = {dimension_name1: length, dimension_name2: length...}
+    kwargs: options (default '')
+            product_version (default 1.0)
+            file_location (default '.')
+    """
+    location = instrument_dict['info']['Mobile/Fixed (loc)'].split('-')[-1].strip().lower()
+    if 'product_version' in kwargs:
+        product_version = kwargs['product_version']
+    else:
+        product_version = 1.0
+    if 'options' in kwargs:
+        no_options = len(kwargs['options'].split('_'))
+        if no_options > 3:
+            msg = f'Too many options, maximum allowed 3, given {no_options}'
+            raise ValueError(msg)
+        options = f"_{kwargs['options']}"
+    else:
+        options = ''
+    if 'file_location' in kwargs:
+        file_location = kwargs['file_location']
+    else:
+        file_location = '.'
+        
+    filename = f"{instrument}_{location}_{time}_{product}{options}_v{product_version}.nc"
+    
+    ncfile = Dataset(f'{file_location}/{filename}', 'w', format='NETCDF4_CLASSIC')
+    created_time = dt.datetime.now(tz=pytz.timezone('UTC')).strftime('%Y-%m-%dT%H:%M:%S')
+    
+    add_attributes(ncfile, instrument_dict, product, created_time, location)
+    add_dimensions(ncfile, instrument_dict, product, dimension_lengths)
+    add_variables(ncfile, instrument_dict, product)
+    
+    ncfile.close()
+    
+
+
+def main(instrument, date = None, dimension_lengths = {}):
+    """
+    Create 'just-add-data' AMOF-compliant netCDF file 
+    
+    Args:
+        instrument (str): ncas instrument name
+        date (str): date for file, format YYYYmmdd. If not given, finds today's date
+        dimension_lengths (dict): dictionary of dimension:length. If length not given 
+                                  for needed dimension, user will be asked to type 
+                                  in dimension length
+    """
+    if date == None:
+        date = dt.datetime.now(dt.timezone.utc).strftime("%Y%m%d")
+    
+    instrument_dict = tsv2dict.instrument_dict(instrument)
+    
+    tsvdictkeys = instrument_dict.keys()
+    products = list(tsvdictkeys)
+    products.remove('info')
+    products.remove('common')
+    
+    all_dimensions = []
+    dimlengths = {}
+    for key, val in instrument_dict.items():
+        if 'dimensions' in val.keys():
+            for dim in list(val['dimensions'].keys()):
+                if dim not in all_dimensions:
+                    all_dimensions.append(dim)
+                    if '<' not in val['dimensions'][dim]['Length']:
+                        dimlengths[dim] = int(val['dimensions'][dim]['Length'])
+    for key, value in dimension_lengths.items():
+        if key not in dimlengths.keys():
+            dimlengths[key] = value
+    for dim in all_dimensions:
+        if dim not in dimlengths.keys():
+            length = input(f"Enter length for dimension {dim}: ")
+            dimlengths[dim] = int(length)
+
+    for product in products:
+        make_netcdf(instrument, product, date, instrument_dict, dimension_lengths = dimlengths)
+    
+    
+if __name__ == "__main__":
+    import sys
+    instrument = sys.argv[1]
+    date = None
+    #date = '20220404'
+    dimlens = {}
+    #dimlens = {'time':96,'altitude':46,'layer_index':45}
+    main(instrument,date=date,dimension_lengths = dimlens)
---
+++ tsv2dict.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import re
+import requests
+
+import values
+
+COMMON_VARIABLES_LAND_URL = f'https://raw.githubusercontent.com/ncasuk/AMF_CVs/{values.TAG}/product-definitions/tsv/_common/variables-land.tsv'
+COMMON_DIMENSIONS_LAND_URL = f'https://raw.githubusercontent.com/ncasuk/AMF_CVs/{values.TAG}/product-definitions/tsv/_common/dimensions-land.tsv'
+COMMON_ATTRIBUTES_URL = f'https://raw.githubusercontent.com/ncasuk/AMF_CVs/{values.TAG}/product-definitions/tsv/_common/global-attributes.tsv'
+INSTRUMENTS_URL = f'https://raw.githubusercontent.com/ncasuk/AMF_CVs/{values.TAG}/product-definitions/tsv/_vocabularies/ncas-instrument-name-and-descriptors.tsv'
+
+
+def tsv2dict_vars(tsv_file):
+    df_vars = pd.read_csv(tsv_file, sep='\t')
+    df_vars=df_vars.fillna('')
+    
+    all_vars_dict = {}
+    current_var_dict = {}
+    first_loop = True
+    current_var = ''
+    
+    for current_line in df_vars.iloc:
+        if current_line['Variable'] != '':
+            if not first_loop:
+                all_vars_dict[current_var] = current_var_dict
+                current_var_dict = {}
+            else:
+                first_loop = False
+            current_var = current_line['Variable']
+            current_var_dict = {}
+            #continue
+        if current_line['Attribute'] != '':
+            current_var_dict[current_line['Attribute']] = current_line['Value']
+    all_vars_dict[current_var] = current_var_dict
+    
+    return all_vars_dict
+
+
+def tsv2dict_dims(tsv_file):
+    df_dims = pd.read_csv(tsv_file, sep='\t')
+    df_dims = df_dims.fillna('')
+    
+    all_dims_dict = {}
+    
+    for dim in df_dims.iloc:
+        dim_dict = dim.to_dict()
+        dim_name = dim_dict.pop('Name')
+        all_dims_dict[dim_name] = dim_dict
+        
+    return all_dims_dict
+
+
+def tsv2dict_attrs(tsv_file):
+    df_attrs = pd.read_csv(tsv_file, sep='\t')
+    df_attrs = df_attrs.fillna('')
+    
+    all_attrs_dict = {}
+    
+    for dim in df_attrs.iloc:
+        dim_dict = dim.to_dict()
+        dim_name = dim_dict.pop('Name')
+        all_attrs_dict[dim_name] = dim_dict
+        
+    return all_attrs_dict
+
+
+def tsv2dict_instruments(tsv_file):
+    df_instruments = pd.read_csv(tsv_file, sep='\t')
+    df_instruments = df_instruments.fillna('')
+    
+    all_instruments = {}
+    
+    for current_instrument in df_instruments.iloc:
+        inst_dict = current_instrument.to_dict()
+        inst_name = inst_dict.pop('New Instrument Name')        
+        data_products = re.split(',| |\|', inst_dict['Data Product(s)'])
+        data_products = list(filter(None, data_products))
+        inst_dict['Data Product(s)'] = data_products        
+        all_instruments[inst_name] = inst_dict
+        
+    return all_instruments
+
+
+def create_variables_tsv_url(product):
+    return f'https://raw.githubusercontent.com/ncasuk/AMF_CVs/{values.TAG}/product-definitions/tsv/{product}/variables-specific.tsv'
+
+
+def create_dimensions_tsv_url(product):
+    return f'https://raw.githubusercontent.com/ncasuk/AMF_CVs/{values.TAG}/product-definitions/tsv/{product}/dimensions-specific.tsv'
+
+
+def create_attributes_tsv_url(product):
+    return f'https://raw.githubusercontent.com/ncasuk/AMF_CVs/{values.TAG}/product-definitions/tsv/{product}/global-attributes-specific.tsv'
+
+
+def instrument_dict(desired_instrument):
+    instrument_dict = {}
+    instrument_dict['info'] = tsv2dict_instruments(INSTRUMENTS_URL)[desired_instrument]
+
+    # Add common stuff
+    instrument_dict['common'] = {}
+    instrument_dict['common']['attributes'] = {}
+    instrument_dict['common']['dimensions'] = {}
+    instrument_dict['common']['variables'] = {}
+
+    instrument_dict['common']['attributes'] = tsv2dict_attrs(COMMON_ATTRIBUTES_URL)
+    instrument_dict['common']['dimensions'] = tsv2dict_dims(COMMON_DIMENSIONS_LAND_URL)
+    instrument_dict['common']['variables'] = tsv2dict_vars(COMMON_VARIABLES_LAND_URL)
+
+    # Add stuff for each product of instrument it specifics exist
+    for product in instrument_dict['info']['Data Product(s)']:
+        instrument_dict[product] = {}
+        instrument_dict[product]['attributes'] = {}
+        instrument_dict[product]['dimensions'] = {}
+        instrument_dict[product]['variables'] = {}
+        
+        attr_url = create_attributes_tsv_url(product)
+        dim_url = create_dimensions_tsv_url(product)
+        var_url = create_variables_tsv_url(product)
+    
+        request = requests.get(attr_url)
+        if request.status_code == 200:
+            instrument_dict[product]['attributes'] = tsv2dict_attrs(attr_url)
+    
+        request = requests.get(dim_url)
+        if request.status_code == 200:
+            instrument_dict[product]['dimensions'] = tsv2dict_dims(dim_url)
+    
+        request = requests.get(var_url)
+        if request.status_code == 200:
+            instrument_dict[product]['variables'] = tsv2dict_vars(var_url)
+
+    return instrument_dict
+
+    
+if __name__ == "__main__":
+    import sys
+    desired_instrument = sys.argv[1]
+    instrument = instrument_dict(desired_instrument)
+    '''
+    This bit just makes the print look pretty, a standard print would also work
+    '''
+    import pprint
+    pp = pprint.PrettyPrinter(indent = 2, width = 200)
+    pp.pprint(instrument)
