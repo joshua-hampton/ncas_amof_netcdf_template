96644613b9bc8f14949260aeb70f1d8bdc440645
Joshua Hampton <joshua.hampton@live.co.uk>
2022-10-03 10:05:29 +0100

    Added example value where no value present for attribute

--- tsv2dict.py
+++ tsv2dict.py
@@ -28,7 +28,10 @@ def tsv2dict_vars(tsv_file):
             current_var_dict = {}
             #continue
         if current_line['Attribute'] != '':
-            current_var_dict[current_line['Attribute']] = current_line['Value']
+            if current_line['Value'] == '' and 'example value' in current_line.keys() and current_line['example value'] != '':
+                current_var_dict[current_line['Attribute']] = f"EXAMPLE: {current_line['example value']}"
+            else:
+                current_var_dict[current_line['Attribute']] = current_line['Value']
     all_vars_dict[current_var] = current_var_dict
     
     return all_vars_dict
