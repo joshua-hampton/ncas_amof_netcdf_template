f6a865aa15dbc382b6db1eb13b7eb2fc65c72eed
Joshua Hampton <joshua.hampton@live.co.uk>
2023-10-31 09:43:37 +0000

    Auto update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Joshua M. Hampton'
 author = 'Joshua M. Hampton'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.2'
+release = '2.3.0'
 
 
 # -- General configuration ---------------------------------------------------
