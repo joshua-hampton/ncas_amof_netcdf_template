98f0e752ad36cac42dd3f9209d7093308e12e739
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2026-05-06 14:21:48 +0100

    Update version number

--- src/ncas_amof_netcdf_template/__about__.py
+++ src/ncas_amof_netcdf_template/__about__.py
@@ -1,1 +1,1 @@
-__version__="2.6.0" # this needs to have no spacing around the "=" due to the workflow that changes it
+__version__="2.6.1" # this needs to have no spacing around the "=" due to the workflow that changes it
